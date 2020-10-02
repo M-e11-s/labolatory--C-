c3d0d02c29031b8f08dbdb51fcf8fb794c531256
M-e11-s <godnessmells@gmail.com>
2020-10-02 12:07:38 +0300

    hotfix №2

--- labolatory №2/lab2.c
+++ labolatory №2/lab2.c
@@ -1,10 +1,12 @@
+#define _USE_Math_Defines
 #include <stdio.h>
 #include <math.h>
 
 int main()
-
+        
 {
         //num_swap
+        printf("PI=%.20f\n", M_PI);
         int num_1,num_2,num_3,num_4;
         printf("Input A:");
         scanf("%d", &num_1);
@@ -58,7 +60,7 @@ int main()
         scanf("%lf", &rad);
                 if (rad>0 && rad<6.2830)
                         {
-                                  deg = rad*180/3.1415; printf("deg=%lf\n", deg);
+                                  deg = rad*180/M_PI; printf("deg=%lf\n", deg);
                         }
                 else
                         {
@@ -95,20 +97,23 @@ int main()
         printf ("S=%.2f \n", S);
 
         float h;
-        h = (2*S)/a1;
+        h = (2*S)/c1;
         printf("h=%.2f \n", h);
 
 
         double rangle1 = cos((pow(a1,2)+pow(b1,2)-pow(c1,2))/(2*a1*b1));
-        double angle1  = (rangle1*180)/3.1415;
+        printf("rangle1=%f\n", rangle1);
+        double angle1  = (rangle1*180)/M_PI;
         printf("angle1=%f \n", angle1);
         double rangle2 = cos((pow(c1,2)+pow(b1,2)-pow(a1,2))/(2*c1*b1));
-        double angle2  = (rangle2*180)/3.1415;
+        printf("rangle2=%f\n", rangle2);
+        double angle2  = (rangle2*180)/M_PI;
         printf("angle2=%f \n", angle2);
         /*double angle3 = 180.0-(angle1+angle2);
         printf("angle3=%f \n", angle3); */
         double rangle3 = cos((pow(c1,2)+pow(a1,2)-pow(b1,2))/(2*c1*a1));
-        double angle3  = (rangle3*180)/3.1415;
+        printf("rangle3=%f\n", rangle3);
+        double angle3  = (rangle3*180)/M_PI;
         printf("angle3=%f \n", angle3);
 
 
@@ -116,7 +121,7 @@ int main()
 
 
                 //2)
-        double h_2,
+        double h_2;
 
 
 
